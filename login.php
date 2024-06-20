9af5f31a29180ebf311bb422487951b656f37e3b
lesdavils <114933667+lesdavils@users.noreply.github.com>
2024-06-20 12:04:40 +0200

    modit

--- login.php
+++ login.php
@@ -5,12 +5,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    if ($email == "admin" && $password == "admin") {
+    if ($email == "admin@pharmatech.com" && $password == "admin") {
         $_SESSION['username'] = "admin";
         header("Location: accueil.php");
         exit();
     } else {
-        $_SESSION['error_message'] = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+        $_SESSION['error_message'] = "L'email ou le mot de passe est incorrect.";
         header("Location: index.php");
         exit();
     }
