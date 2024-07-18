3f00b0e26effecfed12519ea132eb53d4f2e40c3
lesdavils <114933667+lesdavils@users.noreply.github.com>
2024-07-18 11:33:08 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -139,7 +139,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         .form-container {
             max-width: 600px;
             margin: 0 auto;
-            background-color: white;
+            background-color: rgba(255, 255, 255, 0.5);
             padding: 20px;
             border-radius: 5px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
