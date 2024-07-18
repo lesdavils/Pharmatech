99351343fb9a38a3ae38f4e2bbec4cf20dfc060f
lesdavils <114933667+lesdavils@users.noreply.github.com>
2024-07-18 10:45:18 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -70,7 +70,7 @@ $logs = $pdo->query("SELECT * FROM logs ORDER BY timestamp DESC LIMIT 10")->fetc
 
         .container {
             padding: 20px;
-            background-color: rgba(255, 255, 255, 0.9);
+            background-color: rgba(255, 255, 255, 0.3);
             border-radius: 10px;
             box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
             margin: 20px;
