b3db03735802b557c3d229054da7387a67bb94f9
lesdavils <114933667+lesdavils@users.noreply.github.com>
2024-07-18 11:35:51 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -129,6 +129,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             padding: 14px 20px;
             text-decoration: none;
         }
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
         .container {
             padding: 20px;
         }
